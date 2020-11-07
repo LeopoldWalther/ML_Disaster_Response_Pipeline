c658381d738dee456b0a2474611defa74b68628b
LeopoldWalther <Leopold.Walther@gmail.com>
2020-11-07 20:59:22 +0100

    feat: first version of load and preprocess data

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,59 @@
-import sys
-
+# import libraries
+import sys, os
+import pandas as pd
+import numpy as np
+import sqlalchemy
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # merge datasets
+    df = pd.merge(messages, categories, on=['id'])
+    
+    return df
+    
 
 
 def clean_data(df):
-    pass
+    
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(';', expand=True)
+    
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    
+    # use this row to extract a list of new column names for categories.
+    category_colnames = list(row.apply(lambda x: x[:-2]))
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1:])
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+    
+    # drop the original categories column from `df`
+    df.drop(['categories'], axis=1, inplace=True)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = sqlalchemy.create_engine('sqlite:///' + database_filename)
+    df.to_sql('Categorized_Messages', engine, index=False)
 
 
 def main():
@@ -18,6 +61,7 @@ def main():
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
+        print(os.path.abspath(os.getcwd()))
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
         df = load_data(messages_filepath, categories_filepath)
