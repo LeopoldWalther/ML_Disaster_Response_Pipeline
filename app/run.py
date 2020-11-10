80edacd0ab24ef4101f2e3ad75b69fd3e01f6aa6
LeopoldWalther <Leopold.Walther@gmail.com>
2020-11-10 18:30:43 +0100

    refactor: Replace outdated plotly link in master template

--- app/run.py
+++ app/run.py
@@ -11,19 +11,20 @@ from plotly.graph_objs import Bar
 import joblib
 from sqlalchemy import create_engine
 
-
 app = Flask(__name__)
+
 
 def tokenize(text):
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
-
+    
     clean_tokens = []
     for tok in tokens:
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
-
+    
     return clean_tokens
+
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
@@ -33,11 +34,10 @@ df = pd.read_sql_table('Categorized_Messages', con=engine)
 model = joblib.load("../models/classifier.pkl")
 
 
-# index webpage displays cool visuals and receives user input text for model
+# index webpage displays visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
@@ -53,7 +53,7 @@ def index():
                     y=genre_counts
                 )
             ],
-
+            
             'layout': {
                 'title': 'Distribution of Message Genres',
                 'yaxis': {
@@ -71,19 +71,19 @@ def index():
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     
     # render web page with plotly graphs
-    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+    return render_template('master.html', ids=ids, graphJSON=graphJSON, data_set=df)
 
 
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
-
+    query = request.args.get('query', '')
+    
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
-
+    
     # This will render the go.html Please see that file. 
     return render_template(
         'go.html',
